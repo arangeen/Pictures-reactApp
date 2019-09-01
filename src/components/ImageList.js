cab532e5697107aa0e07190b47acecd79d536281
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-09-01 12:04:09 -0700

    Created an ImageCard component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = props => {
   //console.log(props.images);
   //urls & regular comes from each photo property shown in the console
-  const images = props.images.map(({ description, id, urls }) => {
-    return <img alt={description} key={id} src={urls.regular} />;
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
   });
   return <div className="image-list">{images}</div>;
 };
